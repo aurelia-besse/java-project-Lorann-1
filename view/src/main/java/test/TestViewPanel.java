598e39eea68ff802b9b215d7448759321a1ebde7
AdrienKegler <adrien.kegler@viacesi.fr>
2016-06-16 09:35:11 +0200

    Creation of the test classes for the View package

---
+++ view/src/main/java/test/TestViewPanel.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestViewPanel {
+
+	@Test
+	public void testViewPanel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateObservableObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintComponentGraphics() {
+		fail("Not yet implemented");
+	}
+
+}
