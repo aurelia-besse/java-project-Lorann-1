3d1d20ce37d20d229d67d6d4f6f509ac6a55df6f
aurelia-besse <aurelia.besse@viacesi.fr>
2016-06-16 09:20:44 +0200

    commit dbscript

--- DBfillingScript.sql
+++ DBfillingScript.sql
@@ -2,7 +2,18 @@
 #        Script MySQL.
 #------------------------------------------------------------
 
+CREATE DATABASE `databaseProjet` ;
 
+USE `databaseProjet` ;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `elementMap`(IN `NumberMap` INT)
+    NO SQL
+SELECT elementmap.IDelement, elementmap.x, elementmap.y, element.url
+FROM elementmap JOIN element ON elementmap.IDelement=element.IDelement
+WHERE elementmap.IDmap = NumberMap 
+ORDER BY elementmap.y, elementmap.x ASC$$
+
+DELIMITER ;
 #------------------------------------------------------------
 # Table: element
 #------------------------------------------------------------
