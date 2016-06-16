d8a0b6ce392bf681fabf8e28dd2506368cc5eb84
AdrienKegler <adrien.kegler@viacesi.fr>
2016-06-16 09:37:14 +0200

    Creation of the test class for the Controller package

---
+++ controller/src/main/java/Test/TestController.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestController {
+
+	@Test
+	public void testController() {
+		fail("Not yet implemented");
+	}
+
+}
